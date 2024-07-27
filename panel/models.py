47e35576b797b078c5bce82d0e433ed1642df619
Sadra Saderi <sadrassx@gmail.com>
2024-07-27 20:42:58 +0330

    Add edit and delete functionalities

--- panel/models.py
+++ panel/models.py
@@ -16,41 +16,96 @@ class EmailAccount(models.Model):
     class Meta:
         ordering = ["-created_on"]
 
-    # TODO: Throw exceptions if the process was not successful
+    def delete(self, *args, **kwargs):
+        logger = logging.getLogger(__name__)
+
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(MissingHostKeyPolicy())
+        # ssh.load_system_host_keys()
+
+        for _ in range(5):
+            try:
+                ssh.connect(
+                    hostname=settings.POSTFIX_SSH_HOST,
+                    port=(
+                        int(settings.POSTFIX_SSH_PORT)
+                        if settings.POSTFIX_SSH_PORT.isdigit()
+                        else 22
+                    ),
+                    username=settings.POSTFIX_SSH_USER,
+                    password=settings.POSTFIX_SSH_PASSWORD,
+                )
+                break
+            except:
+                # TODO: Throw exception
+                logger.error(traceback.format_exc())
+
+        try:
+            _, output, error = ssh.exec_command("setup email list")
+            users = [line.strip().split()[1] for line in output if line.strip()]
+
+            if self.user in users and len(list(error)) == 0:
+                ssh.exec_command(f"setup email del {self.user}")
+
+            return super().delete(*args, **kwargs)
+        except:
+            # TODO: Throw exception
+            logger.error(traceback.format_exc())
+
     def save(self, *args, **kwargs):
         logger = logging.getLogger(__name__)
 
-        if self._state.adding:
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(MissingHostKeyPolicy())
+        # ssh.load_system_host_keys()
+
+        for _ in range(5):
             try:
-                ssh = SSHClient()
-                # ssh.load_system_host_keys()
-                ssh.set_missing_host_key_policy(MissingHostKeyPolicy())
-
-                for _ in range(5):
-                    try:
-                        ssh.connect(
-                            hostname=settings.POSTFIX_SSH_HOST,
-                            port=(
-                                int(settings.POSTFIX_SSH_PORT)
-                                if settings.POSTFIX_SSH_PORT.isdigit()
-                                else 22
-                            ),
-                            username=settings.POSTFIX_SSH_USER,
-                            password=settings.POSTFIX_SSH_PASSWORD,
-                        )
-                        break
-                    except:
-                        logger.error(traceback.format_exc())
-
-                _, output, error = ssh.exec_command(
-                    f"setup email add {self.user} {self.password}"
+                ssh.connect(
+                    hostname=settings.POSTFIX_SSH_HOST,
+                    port=(
+                        int(settings.POSTFIX_SSH_PORT)
+                        if settings.POSTFIX_SSH_PORT.isdigit()
+                        else 22
+                    ),
+                    username=settings.POSTFIX_SSH_USER,
+                    password=settings.POSTFIX_SSH_PASSWORD,
                 )
-
-                for line in output:
-                    logger.info(line)
-                for line in error:
-                    logger.error(line)
+                break
             except:
+                # TODO: Throw exception
                 logger.error(traceback.format_exc())
 
-        return super().save(*args, **kwargs)
+        try:
+            _, output, error = ssh.exec_command("setup email list")
+            users = [line.strip().split()[1] for line in output if line.strip()]
+
+            output = []
+            error = []
+
+            if self._state.adding and len(list(error)) == 0:
+                if self.user not in users:
+                    _, output, error = ssh.exec_command(
+                        f"setup email add {self.user} {self.password}"
+                    )
+                else:
+                    _, output, error = ssh.exec_command(
+                        f"setup email update {self.user} {self.password}"
+                    )
+            elif self.user in users and len(list(error)) == 0:
+                _, output, error = ssh.exec_command(
+                    f"setup email update {self.user} {self.password}"
+                )
+            else:
+                # TODO: Throw exception
+                logger.error("User not found.")
+
+            for line in output:
+                logger.info(line)
+            for line in error:
+                logger.error(line)
+
+            return super().save(*args, **kwargs)
+        except:
+            # TODO: Throw exception
+            logger.error(traceback.format_exc())
